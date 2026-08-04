4d2ece256c081694c83f2d63fb559523f805a818
Banana Fructa <rndmtbchnnl@gmail.com>
2026-08-05 00:04:31 +0300

    removed dependencies

--- src/main/java/tfcflorae/TFCFlorae.java
+++ src/main/java/tfcflorae/TFCFlorae.java
@@ -77,8 +77,7 @@ public class TFCFlorae
     public static final String DEPENDENCIES = "required-after:tfc@[1.7,);"
             + "after:firmalife;"
             + "after:tfcelementia;"
-            + "after:tfc_ph_compat;"
-            + "required-after:loliasm;";
+            + "after:tfc_ph_compat;";
 
     @Mod.Instance
     public static TFCFlorae instance;
